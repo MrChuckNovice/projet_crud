ebd4096d6da9a303166511e03edb7702ab77c596
aimed <leban.aimed@gmail.com>
2020-11-12 10:55:05 +0100

    test4

--- access.php
+++ access.php
@@ -18,7 +18,7 @@ if($emailform === $emaildb && $mdpform === $mdpdb){
 }
 
 ?> 
-
+<!-- gttgy -->
 <!-- access pour passer a la page suivante grace au localhost aimed/leban -->
 <!-- si sa passe il va  dans le access2 avec le phpmyadmin test_aims/ps5 -->
 <!-- sinon il reste ou il est -->
