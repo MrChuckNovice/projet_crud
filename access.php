2400f25162f36bc62e79c023f63697cf49f8b541
aimed <leban.aimed@gmail.com>
2020-11-12 12:26:50 +0100

    test5

--- access.php
+++ access.php
@@ -3,7 +3,7 @@
 $emailform=$_POST['courrier'];
 $mdpform=$_POST['mdp'];
 
-$pdo=new PDO('mysql:host=localhost;dbname=aimed','root','');
+$pdo=new PDO('mysql:host=localhost;dbname=test_aims','root','');
 $query=$pdo->query('SELECT * FROM leban');
 
 $resultat=$query->fetch(PDO::FETCH_ASSOC);
@@ -18,7 +18,7 @@ if($emailform === $emaildb && $mdpform === $mdpdb){
 }
 
 ?> 
-<!-- gttgy -->
+
 <!-- access pour passer a la page suivante grace au localhost aimed/leban -->
 <!-- si sa passe il va  dans le access2 avec le phpmyadmin test_aims/ps5 -->
 <!-- sinon il reste ou il est -->
